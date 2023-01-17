ca605b962baec5a05c115253478b4a9e032954ee
Aisulu Imankulova <aisulu.imankulova@gmail.com>
2023-01-17 16:40:24 -0500

    modal

--- src/components/RecipeModal.jsx
+++ src/components/RecipeModal.jsx
@@ -1,46 +1,48 @@
-import React from 'react';
-import Modal from '@mui/material/Modal';
-import { Box } from '@mui/material';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Modal from "@mui/material/Modal";
+import { Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
-
-export default function RecipeModal({open, handleClose, ingredientLines, label}) {
+export default function RecipeModal({
+  open,
+  handleClose,
+  ingredientLines,
+  label,
+}) {
   return (
     <div>
-    <Modal
- open={open}
- onClose={handleClose}
- aria-labelledby="modal-modal-title"
- aria-describedby="modal-modal-description"
->
- <Box sx={style}>
-   <Typography id="modal-modal-title" variant="h6" component="h2">
-     Full Recipe of {label}
-   </Typography>
-   <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-     Ingridients: 
-     <ul>{ingredientLines.map(line => {
-       return <li>{line}</li>
-     })}
-     </ul>
-   </Typography>
- </Box>
-</Modal>  
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Full Recipe of {label}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Ingridients:
+            <ul>
+              {ingredientLines.map((line) => {
+                return <li>{line}</li>;
+              })}
+            </ul>
+          </Typography>
+        </Box>
+      </Modal>
     </div>
-  )
+  );
 }
-
-
- 
